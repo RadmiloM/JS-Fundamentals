2975b6e6a09770bb431980bc0888974fff8413a2
RadmiloM <markovicradmilo35@gmail.com>
2024-08-12 22:03:40 +0200

    added div container, order list with list items, text with attributes

--- create-dom.js
+++ create-dom.js
@@ -19,3 +19,29 @@ const bodyElement = document.createElement('body');
 bodyElement.setAttribute('class','dom-body');
 html.appendChild(bodyElement);
 
+// on body element add order list inside div container with name dom-order-list and inside order list add list items with same class attribute and add text
+
+const domOrderList = document.createElement('div');
+html.appendChild(domOrderList);
+domOrderList.setAttribute('class','dom-order-list');
+
+const orderList = document.createElement('ol');
+domOrderList.appendChild(orderList);
+
+const firstItem = document.createElement('li');
+firstItem.setAttribute('class', 'list-item');
+orderList.appendChild(firstItem);
+firstItem.innerText = 'Apple';
+
+const secondItem = document.createElement('li');
+secondItem.setAttribute('class','list-item');
+secondItem.innerText = 'Orange';
+
+orderList.appendChild(secondItem);
+
+const thirdItem = document.createElement('li');
+thirdItem.setAttribute('class','list-item');
+thirdItem.innerText =  'Pineapple';
+
+orderList.appendChild(thirdItem);
+
